14686fb67fa797e48acae95bc45b86f0ea0ca427
aNzTikTok <sshxvpn@proton.me>
2025-05-08 08:51:10 +0800

    Delete applications/luci-app-nfttl directory

--- applications/luci-app-nfttl/files/nfttl.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.nfttl", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/nfttl") then
-		return
-	end
-
-	entry({"admin", "modem", "nfttl"}, cbi("nfttl"), "TTL Control", 100).dependent = true
-end
